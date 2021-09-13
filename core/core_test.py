73283ad6c6d18b3d351793cce6d40fd1ae501994
Jordan Doose <peregrine.developments@gmail.com>
2021-09-13 13:53:20 -0700

    Vector3 and unit tests

--- core/core_test.py
+++ core/core_test.py
@@ -34,6 +34,13 @@ class TestVector2(unittest.TestCase):
         self.assertRaises(TypeError, operator.eq, Vector2(0, 0), 0)
         self.assertRaises(TypeError, operator.eq, Vector2(0, 0), "0")
 
+    def test_neq(self):
+        self.assertFalse(Vector2(0, 0) != Vector2(0, 0))
+        self.assertFalse(Vector2(10, 20) != Vector2(10, 20))
+        self.assertTrue(Vector2(0, 0) != Vector2(10, 20))
+        self.assertRaises(TypeError, operator.ne, Vector2(0, 0), 0)
+        self.assertRaises(TypeError, operator.ne, Vector2(0, 0), "0")
+
     def test_add(self):
         self.assertEqual(Vector2(0, 0) + Vector2(0, 0), Vector2(0, 0))
         self.assertEqual(Vector2(0, 0) + Vector2(2, 3), Vector2(2, 3))
@@ -68,13 +75,13 @@ class TestVector2(unittest.TestCase):
 
     def test_norm(self):
         self.assertEqual(Vector2(0, 0).norm(), 0)
-        self.assertEqual(Vector2(0, 1).norm(), 1)
-        self.assertEqual(Vector2(2, 0).norm(), 2)
+        self.assertEqual(Vector2(1, 0).norm(), 1)
+        self.assertEqual(Vector2(0, 2).norm(), 2)
         self.assertEqual(Vector2(3, 4).norm(), 5)
 
     def test_normalized(self):
-        self.assertEqual(Vector2(0, 1).normalized(), Vector2(0, 1))
-        self.assertEqual(Vector2(2, 0).normalized(), Vector2(1, 0))
+        self.assertEqual(Vector2(1, 0).normalized(), Vector2(1, 0))
+        self.assertEqual(Vector2(0, 2).normalized(), Vector2(0, 1))
         self.assertEqual(Vector2(3, 4).normalized(), Vector2(3/5, 4/5))
         self.assertRaises(ZeroDivisionError, Vector2.normalized, Vector2(0, 0))
 
@@ -91,5 +98,101 @@ class TestVector2(unittest.TestCase):
         self.assertEqual(Vector2(1, 0).cross(Vector2(0, 1)), 1)
         self.assertEqual(Vector2(1, 2).cross(Vector2(3, 4)), -2)
         self.assertRaises(TypeError, Vector2(0, 0).cross, 0)
+
+class TestVector3(unittest.TestCase):
+    def test_init(self):
+        self.assertEqual(Vector3(0, 0, 0).x, 0)
+        self.assertEqual(Vector3(0, 0, 0).y, 0)
+        self.assertEqual(Vector3(0, 0, 0).z, 0)
+        self.assertEqual(Vector3(10, 20, 30).x, 10)
+        self.assertEqual(Vector3(10, 20, 30).y, 20)
+        self.assertEqual(Vector3(10, 20, 30).z, 30)
+        self.assertRaises(TypeError, Vector3, "0", 0, 0)
+        self.assertRaises(TypeError, Vector3, 0, "0", 0)
+        self.assertRaises(TypeError, Vector3, 0, 0, "0")
+
+    def test_str(self):
+        self.assertEqual(str(Vector3(0, 0, 0)), "(0,0,0)")
+        self.assertEqual(str(Vector3(1, 1, 1)), "(1,1,1)")
+        self.assertEqual(str(Vector3(10, 20, 30)), "(10,20,30)")
+
+    def test_repr(self):
+        self.assertEqual(repr(Vector3(0, 0, 0)), "Vector3(0,0,0)")
+        self.assertEqual(repr(Vector3(1, 1, 1)), "Vector3(1,1,1)")
+        self.assertEqual(repr(Vector3(10, 20, 30)), "Vector3(10,20,30)")
+
+    def test_eq(self):
+        self.assertTrue(Vector3(0, 0, 0) == Vector3(0, 0, 0))
+        self.assertTrue(Vector3(10, 20, 30) == Vector3(10, 20, 30))
+        self.assertFalse(Vector3(0, 0, 0) == Vector3(10, 20, 30))
+        self.assertRaises(TypeError, operator.eq, Vector3(0, 0, 0), 0)
+        self.assertRaises(TypeError, operator.eq, Vector3(0, 0, 0), "0")
+
+    def test_neq(self):
+        self.assertFalse(Vector3(0, 0, 0) != Vector3(0, 0, 0))
+        self.assertFalse(Vector3(10, 20, 30) != Vector3(10, 20, 30))
+        self.assertTrue(Vector3(0, 0, 0) != Vector3(10, 20, 30))
+        self.assertRaises(TypeError, operator.ne, Vector3(0, 0, 0), 0)
+        self.assertRaises(TypeError, operator.ne, Vector3(0, 0, 0), "0")
+
+    def test_add(self):
+        self.assertEqual(Vector3(0, 0, 0) + Vector3(0, 0, 0), Vector3(0, 0, 0))
+        self.assertEqual(Vector3(0, 0, 0) + Vector3(2, 3, 4), Vector3(2, 3, 4))
+        self.assertEqual(Vector3(2, 3, 4) + Vector3(4, 5, 6), Vector3(6, 8, 10))
+        self.assertEqual(Vector3(2, 3, 4) + Vector3(4, 5, 6), Vector3(4, 5, 6) + Vector3(2, 3, 4))
+        self.assertRaises(TypeError, operator.add, Vector3(0, 0, 0), 0)
+        self.assertRaises(TypeError, operator.add, Vector3(0, 0, 0), "0")
+
+    def test_sub(self):
+        self.assertEqual(Vector3(0, 0, 0) - Vector3(0, 0, 0), Vector3(0, 0, 0))
+        self.assertEqual(Vector3(0, 0, 0) - Vector3(2, 3, 4), Vector3(-2, -3, -4))
+        self.assertEqual(Vector3(2, 3, 4) - Vector3(4, 5, 6), Vector3(-2, -2, -2))
+        self.assertNotEqual(Vector3(2, 3, 4) - Vector3(4, 5, 6), Vector3(4, 5, 6) - Vector3(2, 3, 4))
+        self.assertRaises(TypeError, operator.sub, Vector3(0, 0, 0), 0)
+        self.assertRaises(TypeError, operator.sub, Vector3(0, 0, 0), "0")
+
+    def test_mul(self):
+        self.assertEqual(Vector3(0, 0, 0) * 0, Vector3(0, 0, 0))
+        self.assertEqual(Vector3(0, 0, 0) * 2, Vector3(0, 0, 0))
+        self.assertEqual(Vector3(1, 2, 3) * 3, Vector3(3, 6, 9))
+        self.assertNotEqual(Vector3(1, 1, 1) * 2, Vector3(1, 1, 1))
+        self.assertRaises(TypeError, operator.mul, Vector3(0, 0, 0), "0")
+        self.assertRaises(TypeError, operator.mul, Vector3(0, 0, 0), Vector3(0, 0, 0))
+
+    def test_div(self):
+        self.assertEqual(Vector3(0, 0, 0) / 1, Vector3(0, 0, 0))
+        self.assertEqual(Vector3(1, 1, 1) / 1, Vector3(1, 1, 1))
+        self.assertEqual(Vector3(2, 2, 2) / 2, Vector3(1, 1, 1))
+        self.assertNotEqual(Vector3(3, 3, 3) / 2, Vector3(3, 3, 3))
+        self.assertRaises(TypeError, operator.truediv, Vector3(0, 0, 0), "0")
+        self.assertRaises(ZeroDivisionError, operator.truediv, Vector3(1, 1, 1), 0)
+
+    def test_norm(self):
+        self.assertEqual(Vector3(0, 0, 0).norm(), 0)
+        self.assertEqual(Vector3(1, 0, 0).norm(), 1)
+        self.assertEqual(Vector3(0, 2, 0).norm(), 2)
+        self.assertEqual(Vector3(0, 0, 3).norm(), 3)
+        self.assertEqual(Vector3(2, 3, 6).norm(), 7)
+
+    def test_normalized(self):
+        self.assertEqual(Vector3(1, 0, 0).normalized(), Vector3(1, 0, 0))
+        self.assertEqual(Vector3(0, 2, 0).normalized(), Vector3(0, 1, 0))
+        self.assertEqual(Vector3(0, 0, 3).normalized(), Vector3(0, 0, 1))
+        self.assertEqual(Vector3(2, 3, 6).normalized(), Vector3(2/7, 3/7, 6/7))
+        self.assertRaises(ZeroDivisionError, Vector3.normalized, Vector3(0, 0, 0))
+
+    def test_dot(self):
+        self.assertEqual(Vector3(0, 0, 0).dot(Vector3(0, 0, 0)), 0)
+        self.assertEqual(Vector3(0, 0, 0).dot(Vector3(1, 1, 1)), 0)
+        self.assertEqual(Vector3(1, 1, 1).dot(Vector3(1, 1, 1)), 3)
+        self.assertEqual(Vector3(1, 2, 3).dot(Vector3(4, 5, 6)), 32)
+        self.assertRaises(TypeError, Vector3(0, 0, 0).cross, 0)
+    
+    def test_cross(self):
+        self.assertEqual(Vector3(0, 0, 0).cross(Vector3(0, 0, 0)), Vector3(0, 0, 0))
+        self.assertEqual(Vector3(0, 0, 0).cross(Vector3(1, 1, 1)), Vector3(0, 0, 0))
+        self.assertEqual(Vector3(1, 0, 0).cross(Vector3(0, 0, 1)), Vector3(0, -1, 0))
+        self.assertEqual(Vector3(1, 2, 3).cross(Vector3(4, 5, 6)), Vector3(-3, 6, -3))
+        self.assertRaises(TypeError, Vector3(0, 0, 0).cross, 0)
 
 unittest.main(argv=[''],verbosity=2, exit=False)
--- core/vector.py
+++ core/vector.py
@@ -210,7 +210,7 @@ class Vector2:
             Normalized vector representation
         """
 
-        return self / self.norm()
+        return (self / self.norm())
 
     def dot(self, other):
         """
@@ -228,7 +228,7 @@ class Vector2:
         """
 
         if isinstance(other, Vector2):
-            return (self.x * other.x) + (self.y * other.y)
+            return ((self.x * other.x) + (self.y * other.y))
         raise TypeError
 
     def cross(self, other):
@@ -247,5 +247,253 @@ class Vector2:
         """
 
         if isinstance(other, Vector2):
-            return (self.x * other.y) - (self.y * other.x)
+            return ((self.x * other.y) - (self.y * other.x))
+        raise TypeError
+
+class Vector3:
+    """
+    A class to represent a three-dimensional vector
+
+    ...
+
+    Attributes
+    ----------
+    x : float
+        x component of the vector
+    y : float
+        y component of the vector
+    z : float
+        z component of the vector
+    
+    Methods
+    -------
+    norm():
+        Calculates and returns the vector's length
+    normalized():
+        Calculates and returns a normalized representation of this vector (length = 1)
+    dot():
+        Calculates the dot product of this vector and another
+    cross():
+        Calculates the cross product of this vector and another
+    """
+
+    def __init__(self, x, y, z):
+        """
+        Construct all necessary attributes for a Vector3 object
+
+        Parameters
+        ----------
+        x : float
+            x component of the vector
+        y : float
+            y component of the vector
+        z : float
+            z component of the vector
+        """
+
+        if not isinstance(x, numbers.Number) or not isinstance(y, numbers.Number) or not isinstance(z, numbers.Number):
+            raise TypeError
+
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        """
+        Return a simple string representation of a Vector3 object
+
+        Returns
+        -------
+        str
+            Simple string representation
+        """
+
+        return ''.join(["(", str(self.x), ",", str(self.y), ",", str(self.z), ")"])
+        
+    def __repr__(self):
+        """
+        Return a comprehensive string representation of a Vector3 object
+
+        Returns
+        -------
+        str
+            Comprehensive string representation
+        """
+        
+        return ''.join(["Vector3(", str(self.x), ",", str(self.y), ",", str(self.z), ")"])
+
+    def __eq__(self, other):
+        """
+        Return the equality of two Vector3 objects
+
+        Parameters
+        ----------
+        other : Vector3
+            Vector3 for right-hand operand in equality
+
+        Returns
+        -------
+        bool
+            Equality of operands
+        """
+
+        if isinstance(other, Vector3):
+            return ((self.x == other.x) and (self.y == other.y) and (self.z == other.z))
+        raise TypeError
+
+    def __ne__(self, other):
+        """
+        Return the inequality of two Vector3 objects
+
+        Parameters
+        ----------
+        other : Vector3
+            Vector3 for right-hand operand in inequality
+
+        Returns
+        -------
+        bool
+            Inequality of operands
+        """
+
+        if isinstance(other, Vector3):
+            return ((self.x != other.x) or (self.y != other.y) or (self.z != other.z))
+        raise TypeError
+
+    def __add__(self, other):
+        """
+        Return a sum of two Vector3 objects
+
+        Parameters
+        ----------
+        other : Vector3
+            Vector3 for right-hand operand in addition
+
+        Returns
+        -------
+        Vector3
+            Componentwise sum of operands
+        """
+
+        if isinstance(other, Vector3):
+            return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+        raise TypeError
+
+    def __sub__(self, other):
+        """
+        Return a minutend of two Vector3 objects
+
+        Parameters
+        ----------
+        other : Vector3
+            Vector3 for right-hand operand in subtraction
+
+        Returns
+        -------
+        Vector3
+            Componentwise minutend of operands
+        """
+
+        if isinstance(other, Vector3):
+            return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+        raise TypeError
+
+    def __mul__(self, other):
+        """
+        Return a scalar multiplication of a Vector3 object
+
+        Parameters
+        ----------
+        other : float
+            Scalar value for right-hand operand in multiplication
+
+        Returns
+        -------
+        Vector3
+            Componentwise scalar multiplication of operands
+        """
+
+        if isinstance(other, numbers.Number):
+            return Vector3(self.x * other, self.y * other, self.z * other)
+        raise TypeError
+
+    def __truediv__(self, other):
+        """
+        Return a scalar division of a Vector3 object
+
+        Parameters
+        ----------
+        other : float
+            Scalar value for right-hand operand in division
+
+        Returns
+        -------
+        Vector3
+            Componentwise scalar division of operands
+        """
+
+        if isinstance(other, numbers.Number):
+            return Vector3(self.x / other, self.y / other, self.z / other)
+        raise TypeError
+
+    def norm(self):
+        """
+        Calculate and return the length of a Vector3 object
+
+        Returns
+        -------
+        int
+            Calculated vector length
+        """
+
+        return math.sqrt(math.pow(self.x, 2) + math.pow(self.y, 2) + math.pow(self.z, 2))
+
+    def normalized(self):
+        """
+        Calculate and return a normalized representation of a Vector3 object
+
+        Returns
+        -------
+        Vector3
+            Normalized vector representation
+        """
+
+        return (self / self.norm())
+
+    def dot(self, other):
+        """
+        Calculate and return the dot product of this Vector3 and another
+
+        Parameters
+        ----------
+        other : Vector3
+            Vector3 for right-hand operand in dot product
+        
+        Returns
+        -------
+        float
+            Resultant dot product
+        """
+
+        if isinstance(other, Vector3):
+            return ((self.x * other.x) + (self.y * other.y) + (self.z * other.z))
+        raise TypeError
+
+    def cross(self, other):
+        """
+        Calculate and return the cross product (scalar) of this Vector3 and another
+
+        Parameters
+        ----------
+        other : Vector3
+            Vector3 for right-hand operand in cross product
+        
+        Returns
+        -------
+        float
+            Resultant cross product
+        """
+
+        if isinstance(other, Vector3):
+            return Vector3((self.y * other.z) - (self.z * other.y), (self.z * other.x) - (self.x * other.z), (self.x * other.y) - (self.y * other.x))
         raise TypeError
